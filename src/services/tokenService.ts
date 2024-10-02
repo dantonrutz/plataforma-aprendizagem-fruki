2fb3b7cabbe13f9df5d711b817b7a94ccd968418
dantonrutz <dantonyuri7@gmail.com>
2024-10-02 08:26:07 -0300

    Removendo dominio do cookie

--- src/services/tokenService.ts
+++ src/services/tokenService.ts
@@ -2,18 +2,14 @@ import { NextRequest, NextResponse } from 'next/server'
 import { cookies } from 'next/headers'
 
 const tokenName = process.env.COOKIE_NAME || ''
-const domainName = process.env.DOMAIN_NAME || ''
 
 const token = {
   saveInCookies(accessToken: string) {
-    console.log('Nome do token - ', tokenName)
-    console.log('Nome do dominio - ', domainName)
     cookies().set({
       name: tokenName,
       value: accessToken,
       maxAge: 6 * 60 * 60 * 1000, // 6 horas em ms
       httpOnly: true,
-      domain: domainName,
       path: '/',
       sameSite: 'lax',
       secure: false,
